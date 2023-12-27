35537c0d72f1df50d0a952c13d91c3d4cfceffd6
Fernando Mercado <fermercado@live.com>
2023-12-27 09:32:12 -0300

    feat: add news routes in event

--- src/routes/EventRoutes.ts
+++ src/routes/EventRoutes.ts
@@ -1,13 +1,25 @@
 import express from 'express';
-import { createEvent, getEvents } from '../controllers/EventController';
+import {
+  createEvent,
+  getEvents,
+  deleteEvents,
+  getEventById,
+  deleteEventById,
+} from '../controllers/EventController';
 import { authenticateUser } from '../middlewares/auth';
 
 const router = express.Router();
 
 router.use(authenticateUser);
 
-router.post('/', createEvent);
+router.post('/', authenticateUser, createEvent);
 
-router.get('/', getEvents);
+router.get('/', authenticateUser, getEvents);
+
+router.get('/:id', authenticateUser, getEventById);
+
+router.delete('/', authenticateUser, deleteEvents);
+
+router.delete('/:id', authenticateUser, deleteEventById);
 
 export default router;

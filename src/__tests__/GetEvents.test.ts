464ac7f6917fe51d970d49b3fb4711d503336c10
Fernando Mercado <fermercado@live.com>
2023-12-30 10:32:51 -0300

    test: upgrade test GetEvents

--- src/__tests__/GetEvents.test.ts
+++ src/__tests__/GetEvents.test.ts
@@ -13,8 +13,9 @@ describe('Get Events', () => {
   let userId: string;
 
   beforeAll(async () => {
-    mongoServer = new MongoMemoryServer();
+    mongoServer = await MongoMemoryServer.create();
     const mongoUri = await mongoServer.getUri();
+
     await mongoose.connect(mongoUri, {});
 
     const passwordHash = await bcrypt.hash('password123', 10);
@@ -28,19 +29,21 @@ describe('Get Events', () => {
       country: 'Brasil',
     });
 
-    userId = user._id;
-    userToken = jwt.sign({ userId: userId }, process.env.JWT_SECRET || '', {
+    userId = user.id;
+    userToken = jwt.sign({ userId }, process.env.JWT_SECRET || '', {
       expiresIn: '1h',
     });
 
     await Event.create([
       { description: 'Event 1', dayOfWeek: 'monday', userId },
       { description: 'Event 2', dayOfWeek: 'tuesday', userId },
+      { description: 'Event 3', dayOfWeek: 'wednesday', userId },
     ]);
   });
 
   afterAll(async () => {
-    await mongoose.disconnect();
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
     await mongoServer.stop();
   });
 
@@ -50,7 +53,8 @@ describe('Get Events', () => {
       .set('Authorization', `Bearer ${userToken}`);
 
     expect(response.status).toBe(200);
-    expect(response.body.length).toBeGreaterThan(0);
+    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body.length).toBe(3);
   });
 
   it('should retrieve events filtered by dayOfWeek', async () => {
@@ -60,19 +64,73 @@ describe('Get Events', () => {
       .set('Authorization', `Bearer ${userToken}`);
 
     expect(response.status).toBe(200);
-    expect(response.body.length).toBeGreaterThan(0);
-    expect(
-      response.body.every((event: any) => event.dayOfWeek === 'monday'),
-    ).toBe(true);
+    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].dayOfWeek).toBe('monday');
   });
 
-  it('should return a message for no events found', async () => {
+  it('should return a message for no events found on a specific day', async () => {
     const response = await request(app)
       .get('/api/v1/events')
       .query({ dayOfWeek: 'sunday' })
       .set('Authorization', `Bearer ${userToken}`);
 
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ message: 'No events on this day.' });
+    expect(response.body).toEqual({
+      message: 'No events found',
+    });
+  });
+
+  it('should retrieve events filtered by description', async () => {
+    const response = await request(app)
+      .get('/api/v1/events')
+      .query({ description: 'Event 1' })
+      .set('Authorization', `Bearer ${userToken}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].description).toBe('Event 1');
+  });
+
+  it('should retrieve only events created by the authenticated user', async () => {
+    const response = await request(app)
+      .get('/api/v1/events')
+      .query({ onlyMyEvents: 'true' })
+      .set('Authorization', `Bearer ${userToken}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body.length).toBe(3);
+  });
+
+  it('should return a message when the authenticated user has no events', async () => {
+    const newUser = await User.create({
+      firstName: 'João',
+      lastName: 'Silva',
+      email: 'joao@gmail.com',
+      password: await bcrypt.hash('password123', 10),
+      birthDate: '1990-01-11',
+      city: 'São Paulo',
+      country: 'Brasil',
+    });
+
+    const newUserToken = jwt.sign(
+      { userId: newUser.id },
+      process.env.JWT_SECRET || '',
+      {
+        expiresIn: '1h',
+      },
+    );
+
+    const response = await request(app)
+      .get('/api/v1/events')
+      .query({ onlyMyEvents: 'true' })
+      .set('Authorization', `Bearer ${newUserToken}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      message: 'No events created by this user.',
+    });
   });
 });

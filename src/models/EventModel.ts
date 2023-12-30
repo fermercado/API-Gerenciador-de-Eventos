fee56f4b577642dc9a57637548b3ee80b9cc87af
Fernando Mercado <fermercado@live.com>
2023-12-30 15:13:15 -0300

    chore: update EventModel

--- src/models/EventModel.ts
+++ src/models/EventModel.ts
@@ -3,24 +3,28 @@ import mongoose, { Schema, Document } from 'mongoose';
 interface Event extends Document {
   description: string;
   dayOfWeek: string;
+  userId: string;
 }
 
-const EventSchema = new Schema({
-  description: { type: String, required: true },
-  dayOfWeek: {
-    type: String,
-    enum: [
-      'sunday',
-      'monday',
-      'tuesday',
-      'wednesday',
-      'thursday',
-      'friday',
-      'saturday',
-    ],
-    required: true,
+const EventSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    dayOfWeek: {
+      type: String,
+      enum: [
+        'sunday',
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'friday',
+        'saturday',
+      ],
+      required: true,
+    },
+    userId: { type: String, required: true },
   },
-  userId: { type: String, required: true },
-});
+  { versionKey: false },
+);
 
 export default mongoose.model<Event>('Event', EventSchema);

2cd691091a7aace0a32e87bf0f58f8c62282a84c
Fernando Mercado <fermercado@live.com>
2024-07-01 10:33:08 -0300

    feat: added hashService

---
+++ src/services/hash/HashService.ts
@@ -0,0 +1,17 @@
+import * as bcrypt from 'bcryptjs';
+
+export class HashService {
+  private saltRounds: number;
+
+  constructor() {
+    this.saltRounds = process.env.NODE_ENV === 'test' ? 1 : 10;
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, this.saltRounds);
+  }
+
+  async comparePassword(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
+}
